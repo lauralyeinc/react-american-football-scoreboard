6a4969a2be5d2bffce9cc75da043d948edd80784
lauralyeinc <52268790+lauralyeinc@users.noreply.github.com>
2019-09-19 15:07:24 -0400

    saving stretch work, wanted to make another button for the quarter area of the scoreboard. Have it almost done, having problems with appendChild with the div/button I made in the DOM and adding it to the button div.

--- src/BottomRow.js
+++ src/BottomRow.js
@@ -1,7 +1,21 @@
 import React from "react";
 import "./App.css";
 
+const [quarter, setQuarter] = React.useState(1);
+
+const quarterChange = event => { 
+  setQuarter(quarter + 1);
+}
+
 const BottomRow = () => {
+
+  // establish bootomRow app's quater state with useState hooks. 
+
+ 
+
+  // const quarterChange needs to stop at 4... tbc. 
+  //line 6 above
+
   return (
     <div className="bottomRow">
       <div className="down">
@@ -18,10 +32,40 @@ const BottomRow = () => {
       </div>
       <div className="quarter">
         <h3 className="quarter__title">Quarter</h3>
-        <div className="quarter__value">4</div>
+        
+        {/* change hardcode value of quater in quarter value div to accept dynamic values from state.*/}
+
+        <div className="quarter__value">{quarter}</div>
+
       </div>
     </div>
+    
   );
 };
+
+
+// goal is to make a new button in the section className buttons under the div of awaybuttons in a div className of gamefuctions in a div called quaterButton.  To do that I would have to make the items in the DOM. 
+function gameFunctionButtons() {
+  // create elements 
+  const gameFunctions = document.createElement('div');
+  const quarterButton = document.createElement('button');
+  
+  //structure elements 
+  gameFunctions.appendChild(quarterButton);
+
+  // add conents to elements
+  quarterButton.textContent = `Set Quater`; 
+
+  // add style
+  quarterButton.classList.add('button');
+
+  quarterButton.addEventListener('click', (e) => {
+    {quarterChange()}
+  })
+
+  return gameFunctions;
+}
+
+ buttons.appendChild(gameFunctions);
 
 export default BottomRow;
